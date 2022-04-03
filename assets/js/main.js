52f16a8b6752bbcb27c23c0a318978fcb7ad3b6e
Gabriel Maugeri <maugerigabriel@gmail.com>
2022-04-03 15:37:39 -0300

    sliders habitaciones y paquetes beta terminados y arreglos menores

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ const nav = document.querySelector("nav");
 
 window.onscroll = function() {
     "use strict";
-    if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
+    if (document.body.scrollTop >= 25 || document.documentElement.scrollTop >= 25) {
         nav.classList.add("scrolled");
     } else {
         nav.classList.remove("scrolled");
@@ -13,53 +13,7 @@ window.onscroll = function() {
 
 
 
-// ======================== MULTI STEP BAR & FORMULARY ========================
-
-const multiStepForm = document.querySelector("[data-multi-step]");
-const formSteps = [...multiStepForm.querySelectorAll("[data-step]")];
-
-const multiStepBar = document.querySelector("[data-multi-step-bar]");
-const barSteps = [...multiStepBar.querySelectorAll("[data-step-bar]")];
 
 
-let currentStep = formSteps.findIndex(step => {
-    return step.classList.contains("active");
-});
 
-
-if (currentStep < 0) {
-    currentStep = 0;
-    formSteps[currentStep].classList.add("active");
-    barSteps[currentStep].classList.add("active");
-}
-
-multiStepForm.addEventListener("click", e => {
-    let incrementor;
-    if (e.target.matches("[data-next]")) {
-        incrementor = 1;
-    } else if (e.target.matches("[data-prev]")) {
-        incrementor = -1;
-    }
-
-    if (incrementor == null) return;
-    
-    const inputs = [...formSteps[currentStep].querySelectorAll("input")];
-    const allValid = inputs.every(input => input.reportValidity());
-    if (allValid) {
-        currentStep += incrementor;
-        showCurrentStep();
-    }
-})
-
-function showCurrentStep() {
-    formSteps.forEach((step, index) => {
-        step.classList.toggle("active", index === currentStep);
-        
-    })
-    barSteps.forEach((step, index) => {
-        step.classList.toggle("active", index === currentStep);
-        step.classList.toggle("completed", index < currentStep);
-        console.log(step);
-    })
-}
 
---
+++ assets/js/stepForm.js
@@ -0,0 +1,49 @@
+// ======================== MULTI STEP BAR & FORMULARY ========================
+
+const multiStepForm = document.querySelector("[data-multi-step]");
+const formSteps = [...multiStepForm.querySelectorAll("[data-step]")];
+
+const multiStepBar = document.querySelector("[data-multi-step-bar]");
+const barSteps = [...multiStepBar.querySelectorAll("[data-step-bar]")];
+
+
+let currentStep = formSteps.findIndex(step => {
+    return step.classList.contains("active");
+});
+
+
+if (currentStep < 0) {
+    currentStep = 0;
+    formSteps[currentStep].classList.add("active");
+    barSteps[currentStep].classList.add("active");
+}
+
+multiStepForm.addEventListener("click", e => {
+    let incrementor;
+    if (e.target.matches("[data-next]")) {
+        incrementor = 1;
+    } else if (e.target.matches("[data-prev]")) {
+        incrementor = -1;
+    }
+
+    if (incrementor == null) return;
+    
+    const inputs = [...formSteps[currentStep].querySelectorAll("input")];
+    const allValid = inputs.every(input => input.reportValidity());
+    if (allValid) {
+        currentStep += incrementor;
+        showCurrentStep();
+    }
+})
+
+function showCurrentStep() {
+    formSteps.forEach((step, index) => {
+        step.classList.toggle("active", index === currentStep);
+        
+    })
+    barSteps.forEach((step, index) => {
+        step.classList.toggle("active", index === currentStep);
+        step.classList.toggle("completed", index < currentStep);
+        console.log(step);
+    })
+}
